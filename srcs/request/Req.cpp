ff9bf1a362edcef1474ad8f70eef9312d7b22092
Gustavo Yuki Miyakawa <gmiyakawa@gmail.com>
2023-11-29 12:46:14 -0500

    envCGI mostly done

--- srcs/request/Req.cpp
+++ srcs/request/Req.cpp
@@ -5,7 +5,7 @@
 
 
 				// depracated due to not using fd
-				// might be creating the body for the response
+				// I think this might actuallly be creating the body for the response
 void Req::body_creation(void)
 {
 	// if (!this->file.is_open())
@@ -30,7 +30,7 @@ Req::Req(std::string HTTP_Req, const int fd, Location &location)
 	if (!_ReqStream.good())
 		fatal ("failed to create request string stream");
 	parseHeader();
-	// setBody();
+
 }
 
 // sets _header and variable. Checks if it's a CGI request. determines which function(GET POST DELETE).
@@ -40,12 +40,85 @@ void	Req::parseHeader(void)
 	while (std::getline(_ReqStream, line) && !line.empty())
 		_header += line + '\n';
 										// cout << "print header: \n\n" << _header << endl << endl;
-	if (_header.find("CGI: true") != string::npos)		// can be replaced for any other way to determine cgi. file .cgi for instance
+	if (_header.find("CGI = true") != string::npos)		// can be replaced for any other way to determine cgi. file .cgi for instance
+	{
 		_isCGI = true;
+		_makeEnvCGI();
+	}
 	else
 		_isCGI = false;
 	parseFirstLine();
+	
 }
+
+void	Req::_makeEnvCGI(void)
+{
+	_populateVarEnvCGI(string("Host"));
+	_populateVarEnvCGI(string("User-Agent"));
+	_populateVarEnvCGI(string("Querry-String"));
+	_populateVarEnvCGI(string("Content-Type"));
+	_populateVarEnvCGI(string("Content-Length"));
+	_populateVarEnvCGI(string("Accept"));
+	_populateVarEnvCGI(string("Accept-Language"));
+	_populateVarEnvCGI(string("Connection"));
+	envCGI["METHOD"] = _method;		// faulty
+	if (_header.find("HTTP/1.0"))
+		envCGI["VERSION"] = "HTTP/1.0";
+	else if (_header.find("HTTP/1.1"))
+		envCGI["VERSION"] = "HTTP/1.1";
+	
+	// MISSING: path name/file name? whats the variable? Could be set prior during verification.
+	
+				// cout << "printing envCGI[QUERRY STRING]:\n" << envCGI["QUERRY_STRING"] << endl;
+	size_t	mapSize = envCGI.size();
+
+	envCGIExecve = new char*[mapSize + 1];
+	envCGIExecve[mapSize + 1] = NULL;	// null-terminating it here, but if uncessary, remove this line and the +1 in the line above
+	size_t	i = 0;
+	std::map<string, string>::iterator it = envCGI.begin();
+	
+	while(it != envCGI.end())
+	{
+		envCGIExecve[i] = new char [it->first.size() + it->second.size() + 4];	// +4 -> 3 for " = " + Null termination
+		std::strcpy(envCGIExecve[i], (it->first + " = " + it->second).c_str());
+		i++;
+		it++;
+	}
+}
+
+void	Req::_populateVarEnvCGI(string var)
+{
+	string				tmp;
+	string::iterator	it;
+	
+	size_t pos = _header.find(var);
+	if (pos != string::npos)
+	{
+		it = _header.begin() + pos;
+		while (*it != '\n' && it != _header.end())
+		{
+			tmp += *it;
+			it++;
+		}
+		envCGI[_formatStringEnvCGI(var)] = tmp;
+	}
+	return;
+}
+
+
+string	Req::_formatStringEnvCGI(string str)
+{
+	string::iterator	it = str.begin();
+	while (it != str.end())
+	{
+		*it = std::toupper(*it);
+		if (*it == '-')
+			*it = '_';
+		it++;
+	}
+	return str;
+}
+
 
 	// checks if the first line is valid, which method is called and if the version is correct.
 void	Req::parseFirstLine(void)
@@ -54,39 +127,64 @@ void	Req::parseFirstLine(void)
 	string				line;
 
 	std::getline(stream, line);
-	if (_hasMultipleMethods(line))
-		fatal ("Invalid HTTP Request");
-
-
+	if (!_validMethod(line))
+		fatal("Invalid HTTP Request");
+	if (!_validPath(line))
+		fatal("Invalid HTTP Request");
+	if (!_validVersion(line))
+		fatal("Invalid HTTP Request");
 }
 
+	// currently doesn't account for spaces. Request must contain
+	// exactly "HTTP/{VERSION}" or br considered invalid.
+bool	Req::_validVersion(string &line)
+{
+	size_t	ver1 = line.find("HTTP/1.1");
+	size_t	ver2 = line.find("HTTP/1.0");
+	if ((ver1 == string::npos && ver2 == string::npos) ||
+		(ver1 != string::npos && ver2 != string::npos))
+		return false;
+	
+	return true;
+}
 
-bool	Req::_hasMultipleMethods(const string &line)
+bool	Req::_validPath(string &line)
+{
+	//	checks if the given path is valid:
+		//check with Location?
+		(void)line;
+	return true;
+}
+
+bool	Req::_validMethod(const string &line)
 {
 	int	count = 0;
 
 	if (line.find("GET") != string::npos)
 	{
-		if (line.find("GEt") != line.rfind("GET"))
-			return true;
+		_method = "GET";
+		if (line.find("GET") != line.rfind("GET"))
+			return false;
 		count++;
 	}
 	if (line.find("POST") != string::npos)
 	{
+		_method = "POST";
 		if (line.find("POST") != line.rfind("POST"))
-			return true;
+			return false;
 		count++;
 	}
 	if (line.find("DELETE") != string::npos)
 	{
+		_method = "DELETE";
 		if (line.find("DELETE") != line.rfind("DELETE"))
-			return true;
+			return false;
 		count++;
 	}
 	if (count != 1)
-		return true;
-	else
 		return false;
+	else
+		return true;
 }
 
 
@@ -125,10 +223,20 @@ void	Req::printReq() {
 	cout << http_Req << endl;
 	cout << "status code: ";
 	cout << status_code << endl;
+	cout << "printing char** CGIenv" << endl;
+	for(int i = 0; envCGIExecve[i] != NULL; i++)
+	{
+		printf("%s\n", envCGIExecve[i]);
+	}
 	cout << "END OF PRINT" << endl;
 }
 
-
 Req::~Req()
 {
+	size_t	mapSize = envCGI.size();
+	for (size_t i = 0; i < mapSize + 1; i++)
+	{
+		delete[] envCGIExecve[i];
+	}
+	delete[] envCGIExecve;
 }
--- srcs/request/Req.hpp
+++ srcs/request/Req.hpp
@@ -17,6 +17,7 @@ private:
 	std::string 			status_line;
 	std::string 			_header;
 	std::string 			_body;
+	string					_method;
 
 	std::string				file_name;		// which file?? html?
 
@@ -26,13 +27,10 @@ private:
 	string					http_Req;
 	std::istringstream		_ReqStream;
 	bool					_isCGI;
-	string					_method;
 	
 	// u_int16_t			methode;
 	typedef u_int16_t 		(Req::*meth)(std::string &element);
 	meth 					methode;
-
-
 
 
 	// status line
@@ -41,10 +39,19 @@ private:
 	u_int16_t 				parsing_status_line(std::vector<std::string> status_line);
 	meth					find_methode(std::string &methode);
 	const std::string		message_status_code(u_int16_t code);
+	
+	
 	// header
 	void					parseHeader(void);
 	void					parseFirstLine(void);
-	bool					_hasMultipleMethods(const string &line);
+	bool					_validMethod(const string &line);
+	bool					_validPath(string &line);
+	bool					_validVersion(string &line);
+	void					_makeEnvCGI(void);
+	string					_formatStringEnvCGI(string str);
+	void					_populateVarEnvCGI(string var);
+	
+	
 	void 					header_creation(void);	// unable to test, more details inside
 	std::string 			cType( void );
 	// body
@@ -55,9 +62,10 @@ private:
 	
 	
 public:
+	std::map<string, string>	envCGI;
+	char						**envCGIExecve;
 	int						fds[2]; // CGI fds
 
-	static					std::vector<std::pair<std::string, std::string> > mime;
 	std::string				getHttpString(void);
 	std::string				get_header()const;
 	u_int16_t				get_status_code() const;
@@ -66,6 +74,9 @@ public:
 
 
 	void					printReq();
+	
+			// what is mime?
+	static					std::vector<std::pair<std::string, std::string> > mime;
 	static void 			innitMime(void);
 	~Req					();
 };
