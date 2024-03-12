daec74dbb51696e8891ff0f18e7eff70993019e0
Gustavo Yuki Miyakawa <gmiyakaw@student.42quebec.com>
2024-03-12 16:45:16 -0400

    merged and cleaned

--- srcs/request/Req_upload.cpp
+++ srcs/request/Req_upload.cpp
@@ -26,27 +26,24 @@ void	Req::parseUpload(void)
 void	Req::createUploadFile()
 {
 	// create upload directory
-	// if (_location.uploadFolder.empty() == false) //if variable is set to be empty / not initialized
-	// {
-	// 	string directoryName = _location.get_upload_folder();
+	if (_location.get_upload_folder().empty() == false) //if variable is set to be empty / not initialized
+	{
+		string directoryName = _location.get_upload_folder();
 
-	// 	// Create the directory using system command
-	// 	std::string command = "mkdir " + directoryName;
-
-	// 	int result = system(command.c_str());
-	// 	if (result != 0)
-	// 	{
-	// 		std::cerr << "error. could not create folder" << endl;
-	// 		set_status_code(INTERNAL_SERVER_ERROR);
-	// 		_error = true;
-	// 		return;
-	// 	}
-	// 	// directoryName.append("/");
-
-	// 	_fileName = directoryName + _fileName;
-	// } else {
-	// 	_fileName = PATH_TO_UPLOAD + _fileName;
-	// }
+		// Create the directory using system command
+		std::string command = "mkdir " + directoryName;
+		int result = system(command.c_str());
+		if (result != 0)
+		{
+			std::cerr << "error. could not create folder" << endl;
+			set_status_code(INTERNAL_SERVER_ERROR);
+			_error = true;
+			return;
+		}
+		_fileName = directoryName + _fileName;
+	} else {
+		_fileName = PATH_TO_UPLOAD + _fileName;
+	}
 
 	FILE	*uploadFile = fopen(_fileName.c_str(), "w");
 
